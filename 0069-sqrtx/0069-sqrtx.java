5ad476d9aeff98bcc0850b86a762b8d5ec03ee7c
manthan4321 <khatrimanthan941@gmail.com>
2026-07-26 19:11:46 +0530

    Time: 1 ms (97.43%), Space: 42.5 MB (62.02%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,13 +1,17 @@
 class Solution {
     public int mySqrt(int x) {
-        // method 1->brute force
-        
-        long root=0;
-        for(long i=0;i<=x;i++){
-            if(i*i>x) break;
-            root=i;
+        // method 2->that is with binary search
+        if(x==0) return 0;
+        int lo=1,hi=x; // ye koi array nhi jo n-1 pe rkheneg
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(mid==x/mid) return mid;
+            else if(mid<x/mid) lo=mid+1; // ye step issliye kiye hai because of integer overflow error or ye kra  hai ki ek chij idhr multiply thi udhr jaake divide hogyi 
+            else hi=mid-1;
         }
-        return (int)root;
+        return hi;
+        
+
         
     }
 }
