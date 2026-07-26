356d13508046b9515a2a029f7a2aa913c4aaf434
manthan4321 <khatrimanthan941@gmail.com>
2026-07-26 17:59:20 +0530

    Time: 0 ms (100%), Space: 46.8 MB (90.31%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,16 +1,38 @@
 class Solution {
     public int maximumCount(int[] nums) {
+        // logic -2 binary search for neg and pos integers
+        // 1st binary search for neg
         int n=nums.length;
-        int pos=0,neg=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]>0){
-                pos++;
+        int lo=0,hi=n-1,idx1=n;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            // koi aisa no jiske piche sare hi neg no ho wo dhudho
+            if(nums[mid]>=0){
+                //mark and go left kyuki kya pta first positive ya zero usse pehle ho]
+                idx1=mid;
+                hi=mid-1;
             }
-            else if(nums[i]<0){
-                neg++;
+            else{
+                lo=mid+1;
             }
         }
-        return Math.max(pos,neg);
+        // now 2nd binary search for positive integers
+        int low=0,high=n-1,idx2=n;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]>0){
+                //mark and go left kya pta usse pehle wla bhi first positive ho
+                idx2=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+                
+            }
+        }
+        int positive=nums.length-idx2;
+        return Math.max(idx1,positive);// n- idx2 issliye kyuki neg nikaaLNE to easy hai jitna idx utne hi -ve no par in case of positive n-idx2
+
         
     }
 }
