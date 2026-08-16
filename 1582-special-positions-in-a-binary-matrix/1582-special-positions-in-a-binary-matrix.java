eb39046b413e29bd093fbbb86c8ff6c00083e52c
manthan4321 <khatrimanthan941@gmail.com>
2026-08-16 16:37:28 +0530

    Time: 5 ms (7.62%), Space: 47.4 MB (13.24%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
@@ -1,5 +1,12 @@
 class Solution {
     public int numSpecial(int[][] mat) {
+        //iss ques ka logic ye hai ki agr hme 1 mil gya to bs merko vo row or vo col check krna hai kya usme koi or 1 to nhi hai 
+        // to jaise hi mujhe mila maine row check krli vo ->kaise?
+        //row check aise ki ki vo row lock krdi or sirf col ko aage bdhata rha 
+        //same goes for col check col lock krdia or row ko aage bdhata rha 
+        // or last me agr rowcount or col count dono hi 1 hai to bs milgya ns
+        //or ek chij or main mistake jo mai kr rha tha to++ variable hai vo bhr aayega if condn ke 
+        
         int m=mat.length;
         int n=mat[0].length;
         int count=0;
@@ -14,7 +21,7 @@ class Solution {
                             rowcount++;
                             
                         }
-                        k++;
+                        k++; // ye variable bhr aayega kyuki agr 0 mil jaye array me to aage hi nhi bdhega ye thats why bhr kia
                     }
                     int col=j;
                     int l=0;
